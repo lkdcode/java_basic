3df5a1b614d7d68f7b89265c271dff16a027444e
lkdcode <dlrlejr1@naver.com>
2023-04-06 14:00:51 +0900

    자바 기초

---
+++ src/day09/quiz/Galaxy.java
@@ -0,0 +1,45 @@
+package day09.quiz;
+
+public class Galaxy extends SmartPhone implements Pencil {
+
+    @Override
+    public String information() {
+        return "갤럭시는 삼성에서 만들어졌고 제원은 다음과 같다.\n"
+                + makeCall() + "\n"
+                + takeCall() + "\n"
+                + takePicture() + "\n"
+                + charge() + "\n"
+                + touchDisplay() + "\n"
+                + "블루투스 펜 탑재 여부: " + bluetoothPen() + "\n";
+    }
+
+    @Override
+    public String charge() {
+        return "고속충전, 고속 무선 충전";
+    }
+
+    @Override
+    public String takePicture() {
+        return "1300만 듀얼카메라";
+    }
+
+    @Override
+    public String makeCall() {
+        return "번호를 누르고 통화버튼을 누름";
+    }
+
+    @Override
+    public String takeCall() {
+        return "전화받기 버튼을 누름";
+    }
+
+    @Override
+    public boolean bluetoothPen() {
+        return true;
+    }
+
+    @Override
+    public String touchDisplay() {
+        return "정전식, 와콤펜 지원";
+    }
+}
